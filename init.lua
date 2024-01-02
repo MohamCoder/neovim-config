9f57bc23138547b4b65b7bdb9a26ad89726a5772
MohamCoder <mohammedmus1000@gmail.com>
2024-01-02 22:05:23 +0300

    add wsl-vim-conf

--- init.lua
+++ init.lua
@@ -1,19 +1,3 @@
-vim.api.nvim_set_keymap('n', 'p', '"+p:%s/\r//g<CR>', {noremap = true})
-vim.api.nvim_set_keymap('v', 'p', '"+p:%s/\r//g<CR>', {noremap = true})
-
-vim.api.nvim_set_keymap('n', 'y', '"+y', {noremap = true})
-vim.api.nvim_set_keymap('v', 'y', '"+y', {noremap = true})
-
-vim.cmd("set autochdir")
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.g.mapleader = " "
-vim.wo.number=true
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -21,12 +5,13 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable", 
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
+require("wsl-vim-conf")
 require("lazy").setup("plugins")
 
---
+++ lua/wsl-vim-conf.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_set_keymap('n', 'p', '"+p:%s/\r//g<CR>', {noremap = true})
+vim.api.nvim_set_keymap('v', 'p', '"+p:%s/\r//g<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', 'y', '"+y', {noremap = true})
+vim.api.nvim_set_keymap('v', 'y', '"+y', {noremap = true})
+vim.cmd("set autochdir")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.wo.number=true
+
