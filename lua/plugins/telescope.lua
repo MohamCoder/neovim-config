dfab6fb59067685837c5a7c45c9b27760ba158c3
MohamCoder <mohammedmus1000@gmail.com>
2024-01-04 20:03:50 +0300

    lsp added

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,24 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.api.nvim_set_keymap('n', '<C-p>', ':lua require("telescope.builtin").find_files({cwd = "/mnt/c/dev/projects"})<CR>', {noremap = true, silent = true})
-    vim.api.nvim_set_keymap('n', '<leader>fg', ':lua require("telescope.builtin").live_grep({cwd = "/mnt/c/dev/projects"})<CR>', {noremap = true, silent = true})
-  end
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      vim.api.nvim_set_keymap('n', '<C-p>', ':lua ("telescope.builtin").find_files({cwd = "/mnt/c/dev/projects"})<CR>', {noremap = true, silent = true})
+      vim.api.nvim_set_keymap('n', '<leader>fg', ':lua ("telescope.builtin").live_grep({cwd = "/mnt/c/dev/projects"})<CR>', {noremap = true, silent = true})
+    end
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,10 +4,13 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-    ensure_installed = { "lua", "javascript", "html" },
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },  
+      ensure_installed = { "lua", "javascript", "html" },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
     })
   end
 }
