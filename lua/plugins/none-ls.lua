fbab71baa7cb6afce58ca1317b4cea8b2a444a12
MohamCoder <mohammedmus1000@gmail.com>
2024-01-18 13:55:28 +0300

    add:eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,13 +2,13 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.quick_lint_js,
-			}
-		})
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d
+    },
+})
 		vim.keymap.set("n", "<Leader>gf", ":lua vim.lsp.buf.format()<CR>", {})
 	end,
 }
