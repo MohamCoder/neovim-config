ae680dd81957d4ad6c67815e751d1430da59fab1
MohamCoder <mohammedmus1000@gmail.com>
2024-02-02 22:56:38 +0300

    fix:ignore node_mod

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "rust_analyzer", "pyright" , "debugpy", "black" },
+        ensure_installed = { "lua_ls", "rust_analyzer", "pyright" , "debugpy" , "black"},
       })
     end,
   },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,33 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      vim.api.nvim_set_keymap('n', '<C-p>',':lua require("telescope.builtin").find_files({ cwd = "/mnt/d/dev/projects" })<CR>', {noremap = true , silent = true})
-      vim.api.nvim_set_keymap('n', '<leader>fg',':lua require("telescope.builtin").live_grep({ cwd = "/mnt/d/dev/projects" })<CR>', {noremap = true , silent = true})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			vim.api.nvim_set_keymap(
+				"n",
+				"<C-p>",
+				':lua require("telescope.builtin").find_files({ cwd = "/mnt/d/dev/projects", file_ignore_patterns = {"node_modules"} })<CR>',
+				{ noremap = true, silent = true }
+			)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>fg",
+				':lua require("telescope.builtin").live_grep({ cwd = "/mnt/d/dev/projects", file_ignore_patterns = {"node_modules"} })<CR>',
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
